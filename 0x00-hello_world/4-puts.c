22b1bc158b641a4c10a7bcc549d151048db759e2
cwayitamjekula <cwayitamjekula@gmail.com>
2023-02-16 16:04:15 -0800

    removed whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - starting point
- *outputs message 
- *Return: 0
- */
+*main - starting point
+*outputs message
+*Return: 0
+*/
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
