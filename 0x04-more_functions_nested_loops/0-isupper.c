aeafdee280bfb667795759e4977d014f1b567dea
cwayitamjekula <cwayitamjekula@gmail.com>
2023-02-23 13:32:27 -0800

    readme, main header, capital letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isupper - show if letter is capital or not
+ *@c: character
+ *Return: void
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
