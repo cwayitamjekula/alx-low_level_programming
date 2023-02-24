4918a64e3384c514220d406b236fc9a666cdae5d
cwayitamjekula <cwayitamjekula@gmail.com>
2023-02-23 16:52:44 -0800

    fixed error

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,7 +13,10 @@ void print_most_numbers(void)
 
 	for (number = 0; number <= 9; number++)
 	{
-		if (number != 2 && number != 4)
+		if (number == 2 || number == 4)
+		{
+		continue;
+		}
 			_putchar(number);
 	}
 	_putchar('\n');
