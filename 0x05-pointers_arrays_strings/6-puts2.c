5477038ae7784e8d3d0caf1eeeab984d89cd7e95
cwayitamjekula <cwayitamjekula@gmail.com>
2023-02-27 17:27:26 -0800

    printing characters of string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *puts2 - prints every character of a string
+ *@str: A pointer to an int that will be changed
+ *
+ *Return: void which means it is correct
+ */
+
+void puts2(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i % 2 == 0)
+_putchar (str[i]);
+}
+
+_putchar ('\n');
+}
