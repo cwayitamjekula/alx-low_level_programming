dd0eee75dc2fea68962a83560757bfccb4a203ce
cwayitamjekula <cwayitamjekula@gmail.com>
2023-03-02 18:59:08 -0800

    fixed error

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 *_strncpy - a function that copies a string.
 *at most n bytes of src are copied.
@@ -20,4 +21,32 @@ for (; bytes < n; bytes++)
 dest[bytes] = '\0';
 
 return (dest);
+}
+#include "main.h"
+/**
+ *_strncat - function that concatenates two strings. it will use
+ * at most n bytes from src. src does not need to be null terminated.
+ *@dest: pointer to destination string.
+ *@src: pointer to source string.
+ *@n: number of bytes to be concatenated.
+ *
+ *Return: pointer to destination string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int length, i;
+/* i is a counter for bytes to be concatenated */
+/* length = length of destination string */
+
+	length = 0;
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++, length++)
+	{
+		dest[length] = src[i];
+	}
+	dest[length] = '\0';
+	return (dest);
 }
