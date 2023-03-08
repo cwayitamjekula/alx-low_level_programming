b5e63fd50f392fc2fdbc41feee6aaab8fff7e2b6
cwayitamjekula <cwayitamjekula@gmail.com>
2023-03-07 19:04:20 -0800

    searching a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches string for set of bytes
+ * @s: This is the C string to be scanned.
+ * @accept:character in str1 that matches one of the characters in str2
+ *Return: string s that matches any character specified in accept
+ **/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s != '\0')
+	{
+		i = 0;
+		while (accept[i] != '\0')
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+
+			i++;
+		}
+
+		s++;
+	}
+	return (0);
+
+}
