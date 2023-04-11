32c953fb8f56f902b852637e9a1f9b021d2e1b29
cwayitamjekula <cwayitamjekula@gmail.com>
2023-04-10 18:59:49 -0700

    fixed error

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,7 +3,8 @@
 /**
  * append_text_to_file - Appends text at the end of a file.
  * standard output.
- * @filename: Name of the file to append. * @text_content: NULL terminated string to write to the file.
+ * @filename: Name of the file to append.
+ * @text_content: NULL terminated string to write to the file.
  * Return: 1 on success, -1 on failure.
  */
 int append_text_to_file(const char *filename, char *text_content)
